ae3a5356391730c886cb677f360eb8af30004f30
mjsacker <mjsacker@gmail.com>
2016-05-23 13:00:23 -0700

    IM DONE U FINISH THIS

--- 1on1soccer/Assets/Scripts/StartGame.cs
+++ 1on1soccer/Assets/Scripts/StartGame.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class StartGame : MonoBehaviour {
-
+	public GameObject Camera;
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey (KeyCode.Space))
 		  {
+			Destroy (Camera);
 			Application.LoadLevel (1);
+			//Camera.transform.position = new Vector3 (0,0,-10f);
 		}
 	}
 
